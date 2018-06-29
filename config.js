4b4411b84488af38df346da0dbb98041686b8cd2
Marty Powell <jpowel25@gmail.com>
2018-06-28 20:40:27 -0400

    added api base

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
 	env: process.env.NODE_ENV || 'development',
 	port: process.env.PORT || 1919,
 	base_url: process.env.BASE_URL || 'http://localhost:1919',
+	api_base: '/api',
 	db: {
 		uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/api',
 	},
